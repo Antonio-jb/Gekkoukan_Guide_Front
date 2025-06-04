b6991bc552e3513384193fb5075e479861e4b21b
antonio-jb <antonio_ajb@outlook.com>
2025-06-04 23:32:36 +0200

    Toques finales

--- app/presentation/views/detail-persona/tab-view/ResistancesPersona.tsx
+++ app/presentation/views/detail-persona/tab-view/ResistancesPersona.tsx
@@ -14,23 +14,23 @@ export const ResistancesPersona = ({weak, resists, reflects, absorbs, nullifies}
     return (
         <View style={styles.containerStats}>
             <View style={styles.row}>
-                <Text style={styles.label}>Weak</Text>
+                <Text style={styles.label}>Weak: </Text>
                 <Text style={styles.value}>{weak}</Text>
             </View>
             <View style={styles.row}>
-                <Text style={styles.label}>Resists</Text>
+                <Text style={styles.label}>Resists: </Text>
                 <Text style={styles.value}>{resists}</Text>
             </View>
             <View style={styles.row}>
-                <Text style={styles.label}>Reflects</Text>
+                <Text style={styles.label}>Reflects: </Text>
                 <Text style={styles.value}>{reflects}</Text>
             </View>
             <View style={styles.row}>
-                <Text style={styles.label}>Absorbs</Text>
+                <Text style={styles.label}>Absorbs: </Text>
                 <Text style={styles.value}>{absorbs}</Text>
             </View>
             <View style={styles.row}>
-                <Text style={styles.label}>Nullifies</Text>
+                <Text style={styles.label}>Nullifies: </Text>
                 <Text style={styles.value}>{nullifies}</Text>
             </View>
         </View>
--- app/presentation/views/detail-persona/tab-view/StatsPersona.tsx
+++ app/presentation/views/detail-persona/tab-view/StatsPersona.tsx
@@ -16,31 +16,31 @@ export const StatsPersona = ({arcana, level, strength, magic, endurance, agility
     return (
         <View style={styles.containerStats}>
             <View style={styles.row}>
-                <Text style={styles.label}>Arcana</Text>
+                <Text style={styles.label}>Arcana: </Text>
                 <Text style={styles.value}>{arcana}</Text>
             </View>
             <View style={styles.row}>
-                <Text style={styles.label}>Level</Text>
+                <Text style={styles.label}>Level: </Text>
                 <Text style={styles.value}>{level}</Text>
             </View>
             <View style={styles.row}>
-                <Text style={styles.label}>Strength</Text>
+                <Text style={styles.label}>Strength: </Text>
                 <Text style={styles.value}>{strength}</Text>
             </View>
             <View style={styles.row}>
-                <Text style={styles.label}>Magic</Text>
+                <Text style={styles.label}>Magic: </Text>
                 <Text style={styles.value}>{magic}</Text>
             </View>
             <View style={styles.row}>
-                <Text style={styles.label}>Endurance</Text>
+                <Text style={styles.label}>Endurance: </Text>
                 <Text style={styles.value}>{endurance}</Text>
             </View>
             <View style={styles.row}>
-                <Text style={styles.label}>Agility</Text>
+                <Text style={styles.label}>Agility: </Text>
                 <Text style={styles.value}>{agility}</Text>
             </View>
             <View style={styles.row}>
-                <Text style={styles.label}>Luck</Text>
+                <Text style={styles.label}>Luck: </Text>
                 <Text style={styles.value}>{luck}</Text>
             </View>
         </View>
